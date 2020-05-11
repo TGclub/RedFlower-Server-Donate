d69e2d9888cf034ddb8bbb7ffe4fd3d53cdfafd4
117503445 <t117503445@gmail.com>
2020-05-11 10:17:38 +0800

    better cors support

--- docker.ps1
+++ docker.ps1
@@ -1,3 +1,3 @@
-docker rm redflowerdonate
+docker rm redflowerdonate -f
 docker build -t 117503445/redflowerdonate .
 docker run --name redflowerdonate --restart=always -d -p 8000:8000 117503445/redflowerdonate 
